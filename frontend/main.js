b6b15dbb3a6217aade995a5cf2c90b5f34d6adb3
devFilipi <filipiserpa.ti@gmail.com>
2024-11-15 22:11:57 -0300

    Alteração na injeção de script para eventos no site.

--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,2 @@
 import "./assets/css/style.css";
 import "regenerator-runtime";
-
-
-import Home from './assets/modules/Home';
-import Login from './assets/modules/Login';
-import Register from "./assets/modules/Register";
-
-const home = new Home(document);
-const login = new Login(document);
-const register = new Register(document);
-
-try { 
-    home.init();
-    login.init();
-    register.init();
-} catch(e) {
-    console.log(e);
-}
